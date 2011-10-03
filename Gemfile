457b9cf5452d173a9655a37983c945ffd448f468
Tom Ligda <tligda@ncomputing.com>
2011-10-03 16:54:07 -0700

    Done with user edit/update, index and destroy actions

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.0.3'
+gem 'rails', '3.0.9'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
@@ -8,14 +8,16 @@ gem 'rails', '3.0.3'
 # gem 'sqlite3-ruby', :require => 'sqlite3'
 gem 'sqlite3-ruby', '1.3.2', :require => 'sqlite3'
 gem 'gravatar_image_tag', '1.0.0.pre2'
+gem 'will_paginate', '3.0.pre2'
 
 group :development do
-  gem 'rspec-rails', '2.3.0'
-  gem 'annotate-models', '1.0.4'
+  gem 'rspec-rails', '2.6.1'
+  gem 'annotate', '2.4.0'
+  gem 'faker', '0.3.1'
 end
 
 group :test do
-  gem 'rspec', '2.3.0'
+  gem 'rspec-rails', '2.6.1'
   gem 'webrat', '0.7.1'
   gem 'factory_girl_rails', '1.0'
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,8 @@
 module SessionsHelper
 
   def sign_in(user)
-    cookies.permanent.signed[:remember_token] = [user.id, user.salt]
+#    cookies.permanent.signed[:remember_token] = [user.id, user.salt]
+    session[:user_id] = user.id
     self.current_user = user
   end
 
@@ -10,7 +11,8 @@ module SessionsHelper
   end
 
   def current_user
-    @current_user ||= user_from_remember_token
+#    @current_user ||= user_from_remember_token
+    @current_user ||= User.find_by_id(session[:user_id])
   end
 
   def signed_in?
@@ -18,8 +20,23 @@ module SessionsHelper
   end
 
   def sign_out
-    cookies.delete(:remember_token)
+#    cookies.delete(:remember_token)
+    session[:user_id] = nil
     self.current_user = nil
+  end
+
+  def current_user?(user)
+    user == current_user
+  end
+
+  def deny_access
+    store_location
+    redirect_to signin_path, :notice => "Please sign in to access this page."
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
   end
 
   private
@@ -29,6 +46,15 @@ module SessionsHelper
     end
 
     def remember_token
-      cookies.signed[:remember_token] || [nil, nil]
+#      cookies.signed[:remember_token] || [nil, nil]
+      session[:user_id] || [nil, nil]
+    end
+
+    def store_location
+      session[:return_to] = request.fullpath
+    end
+
+    def clear_return_to
+      session[:return_to] = nil
     end
 end
